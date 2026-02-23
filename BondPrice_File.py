c657efc95550fb4efb9282ac01efcf431691765e
yuepinglin43 <484641582@qq.com>
2026-02-23 18:15:43 -0500

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -11,13 +11,3 @@ def getBondPrice(y, face, couponRate, m, ppy=1):
     return(bondPrice)
 
 
-y = 0.03
-face = 2000000
-couponRate = 0.04
-m = 10
-
-price1 = getBondPrice (y, face, couponRate, m, ppy=1)
-print(price1)
-price2 = getBondPrice (y, face, couponRate, m, ppy=2)
-print(price2)
-
